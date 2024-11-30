1d836d4fd31c2a545181c8bb665f2897060d2e73
fab1an12 <fabianmiulescu@gmail.com>
2024-11-30 22:06:11 +0100

    Minor Changes

--- Functions/CleanData/cleanDistrictName.py
+++
@@ -1,7 +0,0 @@
-import re
-
-def limpiar_nombre_barrio(nombre):
-    nombre = nombre.lower()
-    nombre = nombre.replace("ï", "i")
-    nombre = re.sub(r"[^a-z0-9\s-]", "", nombre)
-    return nombre.replace(" ", "-")
