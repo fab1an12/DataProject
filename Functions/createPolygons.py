d659d1c466f2e992b88d7ed24ef323e43776a5be
fab1an12 <fabianmiulescu@gmail.com>
2024-12-01 12:13:55 +0100

    Streamlitt

---
+++ Functions/createPolygons.py
@@ -0,0 +1,33 @@
+import json
+import psycopg2
+
+
+db_config = {
+    "dbname": "dataproject",
+    "user": "postgres",
+    "password": "Welcome01",
+    "host": "localhost",
+    "port": 5432
+}
+
+def extraer_poligonos_sql():
+    connection = psycopg2.connect(**db_config)
+    
+    try:
+        with connection.cursor() as cursor:
+            query = "SELECT geo_shape FROM distritos"
+            cursor.execute(query)
+                
+            # Crear una lista solo con los polígonos
+            polygons = []
+            for row in cursor.fetchall():
+                geo_shape = row[0]
+                # Parsear el campo geo_shape desde JSON
+                geo_shape_dict = json.loads(geo_shape)
+                # Extraer coordenadas y añadirlas a la lista de polígonos
+                coordenadas = geo_shape_dict["coordinates"]
+                polygons.extend(coordenadas)  # Agrega directamente cada sublista de coordenadas
+            
+            return polygons
+    finally:
+        connection.close()
--- Functions/main.py
+++ Functions/main.py
@@ -1,33 +1,198 @@
 import streamlit as st
-import matplotlib.pyplot as plt
+import pydeck as pdk
+import pandas as pd
+from createPolygons import extraer_poligonos_sql
 
-# Configuración de Streamlit
-st.title("Predicción de Precios de Vivienda por Barrio")
+# Configuración de la página
+st.set_page_config(page_title="Calcula tu casa ideal", layout="wide", initial_sidebar_state="expanded")
 
-# URI de MongoDB
-uri = "mongodb+srv://fabianmiulescu:DataProject2024@cluster0.dzrgu.mongodb.net/?retryWrites=true&w=majority&tlsAllowInvalidCertificates=true"
+# Estilos globales y personalizados
+st.markdown(
+    """
+    <style>
+    /* Fondo de la página */
+    html, body, [data-testid="stAppViewContainer"], [data-testid="stApp"] {
+        background-color: #1c1c1c;
+        color: white;
+        font-family: 'Roboto', sans-serif;
+    }
+    
+    /* Título principal */
+    h1 {
+        font-size: 2.5rem;
+        color: #4CAF50;
+        text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
+        margin-bottom: 20px;
+        text-align: center;
+    }
+    
+    /* Contenedor centrado */
+    .center-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        margin-top: 20px;
+    }
 
-# Nombre de la base de datos
-database_name = 'precios_vivienda'
+    /* Subtítulos */
+    h2, h3 {
+        color: #ffffff;
+        font-weight: 600;
+        margin-bottom: 15px;
+    }
 
-# Barrio seleccionable
-barrios = [
-    "algiros", "benicalap", "benimaclet", "camins-al-grau", "campanar", "ciutat-vella",
-    "el-pla-del-real", "extramurs", "jesus", "leixample", "lolivereta", "la-saidia",
-    "patraix", "poblats-maritims", "quatre-carreres", "rascanya"
-]
-barrio = st.selectbox("Seleccione un barrio:", barrios)
+    /* Personalización de la barra lateral */
+    [data-testid="stSidebar"] {
+        background-color: #1c1c1c;
+        border-right: 2px solid #4CAF50;
+        box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.3);
+        padding-top: 20px;
+    }
+    [data-testid="stSidebar"] h2, [data-testid="stSidebar"] label {
+        color: #4CAF50;
+        font-weight: bold;
+    }
 
-# Llamar a la función principal
-if st.button("Predecir y Mostrar Gráfica"):
-    try:
-        # Llamar a la función para obtener la gráfica
-        grafico = 1
+    /* Botón personalizado */
+    .stButton>button {
+        display: block;
+        margin: 20px auto;
+        padding: 15px 25px;
+        background: linear-gradient(90deg, #32a852, #3dc973);
+        color: white;
+        font-size: 18px;
+        font-weight: bold;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+        border: none;
+        border-radius: 12px;
+        box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1), 0px 1px 3px rgba(0, 0, 0, 0.06);
+        cursor: pointer;
+        transition: all 0.3s ease;
+    }
+    .stButton>button:hover {
+        background: linear-gradient(90deg, #3dc973, #32a852);
+        transform: translateY(-2px);
+        color: #ffffff;
+        box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.2), 0px 4px 6px rgba(0, 0, 0, 0.1);
+    }
+    .stButton>button:active {
+        transform: translateY(0);
+        box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1), 0px 1px 3px rgba(0, 0, 0, 0.06);
+    }
 
-        # Mostrar la gráfica en Streamlit
-        st.pyplot(grafico)
+    /* Selectbox personalizado */
+    div[data-baseweb="select"] {
+        border-radius: 8px !important;
+        border: 1px solid #4CAF50 !important;
+        background: #4CAF50 !important;
+        color: #4CAF50 !important;
+        font-size: 16px !important;
+        font-weight: bold !important;
+        padding: 2px !important;
+    }
 
-    except ValueError as e:
-        st.error(f"Error en los datos: {e}")
-    except Exception as e:
-        st.error(f"Error inesperado: {e}")
+    /* Checkbox personalizado */
+    .stCheckbox>div {
+        display: flex;
+        align-items: center;
+    }
+    .stCheckbox>div>label {
+        color: #4CAF50;
+        font-weight: bold;
+        font-size: 16px;
+    }
+    .stCheckbox>div>input {
+        width: 20px;
+        height: 20px;
+        margin-right: 10px;
+        cursor: pointer;
+        border: 2px solid #4CAF50;
+        border-radius: 4px;
+        transition: all 0.3s ease;
+    }
+    .stCheckbox>div>input:checked {
+        background: #4CAF50;
+        border-color: #45a049;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+# Título principal
+st.markdown("<h1>Calcula tu Casa Ideal</h1>", unsafe_allow_html=True)
+
+# Barra lateral
+st.sidebar.header("Opciones")
+alquiler = st.sidebar.checkbox("Alquiler")
+compra = st.sidebar.checkbox("Compra")
+
+st.sidebar.header("Preferencias")
+habitaciones = st.sidebar.selectbox("Número de habitaciones", [1, 2, 3, 4, 5, 6, 7, 8, 9], key="habitaciones")
+baños = st.sidebar.selectbox("Número de baños", [1, 2, 3, 4, 5, 6, 7, 8, 9], key="baños")
+parking = st.sidebar.selectbox("Plazas de parking", [1, 2, 3, 4, 5, 6, 7, 8, 9], key="parking")
+
+# Botón funcional
+calcular_btn = st.sidebar.button("Calcular tu casa ideal")
+
+# Mapa centrado en Valencia con tamaño ajustado
+st.markdown('<div class="center-container">', unsafe_allow_html=True)
+map_data = pd.DataFrame({
+    'lat': [39.4699],
+    'lon': [-0.3763]
+})
+polygons = extraer_poligonos_sql()
+
+# Formatear los datos para Pydeck
+geojson_features = []
+for polygon in polygons:
+    geojson_features.append({
+        "type": "Feature",
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [polygon]  # Encapsular en otra lista
+        },
+        "properties": {}
+    })
+
+geojson_data = {
+    "type": "FeatureCollection",
+    "features": geojson_features
+}
+
+# Crear la capa para Pydeck
+layer = pdk.Layer(
+    "GeoJsonLayer",
+    geojson_data,
+    pickable=True,
+    stroked=True,
+    filled=True,
+    lineWidthScale=20,
+    lineWidthMinPixels=1,
+    get_line_color=[0, 160, 0],
+    get_fill_color=[76, 175, 80,50],
+)
+# Crear el mapa con PyDeck
+st.pydeck_chart(pdk.Deck(
+    map_style='mapbox://styles/mapbox/light-v9',
+    initial_view_state=pdk.ViewState(
+        latitude=39.4659,
+        longitude=-0.3763,
+        zoom=12.40,
+        pitch=0,
+    ),
+    layers=[layer],
+), width=1200, height=800, use_container_width=True)
+st.markdown('</div>', unsafe_allow_html=True)
+
+# Acción del botón
+if calcular_btn:
+    st.subheader("Resultado")
+    tipo_vivienda = "alquiler" if alquiler else "compra" if compra else "ninguno"
+    st.write(f"Has elegido una casa para **{tipo_vivienda}** con:")
+    st.write(f"- **{habitaciones} habitaciones**")
+    st.write(f"- **{baños} baños**")
+    st.write(f"- **{parking} plazas de parking**")
+    st.write("Gracias por utilizar nuestra herramienta para calcular tu casa ideal.")
